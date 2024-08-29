6a00fb3f398ae49c107399c6bd7cb9c34f0445c2
RaphaelPortailler <raphael.portailler@lapiscine.pro>
2024-08-29 10:45:15 +0200

    modification du builder afin de permettre a l'admin de choisir un role lors de la création d'un utilisateur

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -3,15 +3,32 @@
 namespace App\Form;
 
 use App\Entity\User;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\RadioType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+
 class UserType extends AbstractType
+
 {
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+
+        $currentUser = $this->security->getUser();
+
         $builder
             ->add('Pseudo')
             //->add('roles')
@@ -22,9 +39,32 @@ class UserType extends AbstractType
             ->add('submit', SubmitType::class,  [
                 'label' => 'S\'inscrire',
                 'attr' => ['class' => 'inscrire']
-            ])
-        ;
+            ]);
+
+
+        if ($currentUser && in_array('ROLE_ADMIN', $currentUser->getRoles())) {
+            $builder->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'Administrateur' => 'ROLE_ADMIN',
+                    'Utilisateur' => 'ROLE_USER',
+                ],
+                'expanded' => false,
+                'multiple' => false,
+                'label' => 'Choisir un rôle',
+            ]);
+
+            $builder->get('roles')->addModelTransformer(new CallbackTransformer(
+                function ($rolesArray) {
+                    return count($rolesArray) > 0 ? $rolesArray[0] : null;
+                },
+                function ($roleString) {
+                    return [$roleString];
+                }
+            ));
+
+        }
     }
+
 
     public function configureOptions(OptionsResolver $resolver): void
     {
